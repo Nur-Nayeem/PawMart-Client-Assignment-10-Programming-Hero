fb09db2db1030d2cae958a807c6fbc1c995fd956
nur-nayeem <nurnayem768@gmail.com>
2025-11-09 02:46:38 +0600

    make navbar fixed at the top

--- src/Layoutes/MainLayout.jsx
+++ src/Layoutes/MainLayout.jsx
@@ -5,10 +5,12 @@ import Navbar from "../components/Navbar";
 const MainLayout = () => {
   return (
     <div className="gradient-bg">
-      <header className="container mx-auto">
-        <Navbar />
+      <header className="fixed top-2 w-full z-50">
+        <nav className="container mx-auto">
+          <Navbar />
+        </nav>
       </header>
-      <main className="container mx-auto mt-10">
+      <main className="container mx-auto pt-30">
         <Outlet />
       </main>
       <footer>Footer</footer>
